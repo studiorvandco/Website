3adfdc4c3bd53817f7f0af5f36d5eae2d65ecd07
Minarox <mathis.sema@gmail.com>
2021-08-07 01:56:37 +0200

    Add playlist page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
+import Playlist from "@/views/Playlist";
 
 const routes = [
   { path: '/', name: 'home', component: Home, meta: { scrollToTop: true }},
+  { path: '/playlist/:id', name: 'playlist', component: Playlist, meta: { scrollToTop: true }},
 ]
 
 const router = createRouter({
---
+++ src/views/Playlist.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="playlist">
+    <img id="banner" :src="YT_Playlists[id]['thumbnail']['maxres']['url']" :alt="$t(YT_Playlists[id]['infos']['title'])">
+    <h1>{{ $t(YT_Playlists[id]['infos']['title']) }}</h1>
+    <p>{{ $t(YT_Playlists[id]['infos']['description']) }}</p>
+
+    <article>
+      <section v-for="i in YT_Playlists[id]['videos'].length" :key="i">
+        <img :src="YT_Playlists[id]['videos'][i-1]['thumbnails']['medium']['url']" :alt="$t(YT_Playlists[id]['infos']['title']+'_'+(i-1))">
+        <h2>{{ $t(YT_Playlists[id]['infos']['title']+'_'+(i-1)) }}</h2>
+        <p>{{ $t(YT_Playlists[id]['infos']['description']+'_'+(i-1)) }}</p>
+        <p>{{ YT_Playlists[id]['videos'][i-1]['statistics'] }}</p>
+      </section>
+    </article>
+
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  },
+  methods: {
+    goto(url) {
+      window.location = url;
+    }
+  },
+  computed: {
+    ...mapState(['YT_Playlists'])
+  }
+}
+</script>
+
+<style scoped>
+#banner {
+  width: 100%;
+  height: 650px;
+  margin-top: -70px;
+  object-fit: cover;
+}
+</style>
