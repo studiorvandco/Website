195d058f7c15a583c46fc379eb1670e67e7f6a2f
Minarox <mathis.sema@gmail.com>
2021-08-07 01:56:12 +0200

    Move Presentation to component

---
+++ src/components/Presentation.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="presentation">
+    <section>
+      <img id="banner" alt="Vue logo" src="@/assets/DSCN8325.jpg">
+      <div>
+        <h1>{{ $t('motto') }}</h1>
+        <button class="btn primary-btn" type="button" @click="() => $router.push('/#association')">{{ $t('read_more') }}</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Productions',
+  methods: {
+    goto(url) {
+      window.location = url;
+    }
+  },
+}
+</script>
+
+<style scoped>
+#banner {
+  width: 100%;
+  height: 650px;
+  margin-top: -70px;
+  object-fit: cover;
+}
+
+#presentation section {
+  z-index: 0;
+  position: relative;
+  margin-bottom: -4px;
+}
+
+#presentation section div {
+  padding: 0 12vw;
+  position: absolute;
+  bottom: 70px;
+}
+
+#presentation section h1 {
+  margin: 0 0 14px;
+  font-size: 2.4em;
+  text-transform: uppercase;
+}
+
+@media screen and (max-width: 600px) {
+  #presentation section div {
+    padding: 0 3vw;
+  }
+}
+
+@media screen and (max-width: 450px) {
+  #presentation section div h1 {
+    text-align: center;
+  }
+
+  #presentation section div button {
+    width: 100%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,6 @@
 <template>
   <article id="home">
-    <section>
-      <img id="banner" alt="Vue logo" src="@/assets/DSCN8325.jpg">
-      <div>
-        <h1>{{ $t('motto') }}</h1>
-        <button class="btn primary-btn" type="button" @click="goto('#association')">{{ $t('read_more') }}</button>
-      </div>
-    </section>
-
+    <Presentation />
     <Productions />
     <Association />
     <Contact />
@@ -15,6 +8,7 @@
 </template>
 
 <script>
+import Presentation from "@/components/Presentation";
 import Productions from "@/components/Productions";
 import Association from "@/components/Association";
 import Contact from "@/components/Contact";
@@ -22,6 +16,7 @@ import Contact from "@/components/Contact";
 export default {
   name: 'Home',
   components: {
+    Presentation,
     Productions,
     Association,
     Contact
@@ -33,46 +28,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-#banner {
-  width: 100%;
-  height: 650px;
-  margin-top: -70px;
-  object-fit: cover;
-}
-
-#home section {
-  z-index: 0;
-  position: relative;
-  margin-bottom: -4px;
-}
-
-#home section div {
-  padding: 0 12vw;
-  position: absolute;
-  bottom: 70px;
-}
-
-#home section h1 {
-  margin: 0 0 14px;
-  font-size: 2.4em;
-  text-transform: uppercase;
-}
-
-@media screen and (max-width: 600px) {
-  #home section div {
-    padding: 0 3vw;
-  }
-}
-
-@media screen and (max-width: 450px) {
-  #home section div h1 {
-    text-align: center;
-  }
-
-  #home section div button {
-    width: 100%;
-  }
-}
-</style>
