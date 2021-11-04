59874e6054ad4e29aafba367d58bd4196eeb6d11
Mathis <mathis.sema@gmail.com>
2021-11-04 01:19:41 +0100

    Add axios library

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 createApp(App)
     .use(store)
+    .use(VueAxios, axios)
     .use(i18n)
     .use(router)
     .mount('#app')
