69ef0acc71c3eea1250f0049361aa65a04ec1120
Mathis <contact@minarox.fr>
2022-08-06 20:47:26 +0200

    Add scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,37 @@ import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
+  },
+  {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, SavedPosition) {
+    if (to.hash) {
+      let id = window.location.href.split("#")[1];
+      if (id.length) {
+        let yOffset = -90;
+        let element = document.getElementById(id);
+        if (element) {
+          let y =
+            element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+          return { top: y, behavior: "smooth" };
+        }
+      }
+    } else if (SavedPosition) {
+      return SavedPosition;
+    } else {
+      return { top: 0, behavior: "smooth" };
+    }
+  },
 });
 
 export default router;
