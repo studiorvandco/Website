27153f71b635916d284a5aa12d39822967efb700
Mathis <contact@minarox.fr>
2022-08-06 20:47:41 +0200

    Add libraries

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import i18n from "./i18n";
+import VueObserveVisibility from "vue-observe-visibility";
+import VueSplide from "@splidejs/vue-splide";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faYoutube,
+  faTwitter,
+  faInstagram,
+  faTwitch,
+} from "@fortawesome/free-brands-svg-icons";
 
-createApp(App).use(router).mount("#app");
+library.add(faYoutube, faTwitter, faInstagram, faTwitch);
+
+createApp(App)
+  .use(i18n)
+  .use(router)
+  .use(i18n)
+  .use(VueObserveVisibility)
+  .use(VueSplide)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
