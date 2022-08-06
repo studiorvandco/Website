06885d4c48904f0e694ec3a6a21620ad14017c5a
Mathis <contact@minarox.fr>
2022-08-06 20:47:07 +0200

    Add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <footer>
+    <p>
+      &copy; {{ currentYear }} &nbsp;|&nbsp;
+      <a href="https://rvandco.fr" class="">RvAndCo.fr</a>
+    </p>
+    <p>
+      Site Web créé par
+      <a href="https://github.com/Minarox" target="_blank" rel="noopener">
+        Minarox </a
+      >.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+  computed: {
+    currentYear() {
+      return new Date().getFullYear().toString();
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/mixin.scss";
+
+footer {
+  @include full-width-background-color(var(--bg-color));
+  @include max-width(var(--max-width-content));
+  padding: 0.8rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  color: var(--subtext-color);
+
+  a {
+    transition: color 200ms;
+    text-decoration: none;
+    color: var(--subtext-color);
+
+    &:hover {
+      color: var(--text-color);
+    }
+  }
+
+  p:last-of-type {
+    justify-self: end;
+  }
+}
+</style>
