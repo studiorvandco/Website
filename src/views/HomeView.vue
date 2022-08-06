70e6dca49f659db17cc3efe01e6791dd2b90d3ef
Mathis <contact@minarox.fr>
2022-08-06 20:53:03 +0200

    Add homepage with i18n

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <article>
+      <h2>Ouais, on produit des trucs 😎</h2>
+    </article>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style scoped lang="scss">
+@import "@/assets/mixin.scss";
+
+main {
+  background-image: url("../assets/img/background.webp");
+  background-attachment: fixed;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: var(--spacing);
+
+  article {
+    @include max-width(var(--max-width-content));
+  }
+}
+</style>
