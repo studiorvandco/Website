ce8984d86533d966bf72530abd490d2da0d94824
Mathis <contact@minarox.fr>
2022-08-16 14:58:01 +0200

    Remove VueObserverVisibility

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
-import VueObserveVisibility from "vue-observe-visibility";
 import VueSplide from "@splidejs/vue-splide";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -18,7 +17,6 @@ library.add(faYoutube, faTwitter, faInstagram, faTwitch);
 createApp(App)
   .use(router)
   .use(i18n)
-  .use(VueObserveVisibility)
   .use(VueSplide)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
