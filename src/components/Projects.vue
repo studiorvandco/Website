2de8a842021aef84fa4ad1d0bb794b25c6bee4cb
Minarox <contact@minarox.fr>
2022-08-21 00:44:54 +0200

    Add custom design to carousel

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,51 +2,76 @@
   <article id="projects">
     <!-- Title -->
     <h2>{{ $t("projects.self") }}</h2>
-    <Splide :options="options" :aria-label="$t('projects.self')">
-      <SplideSlide>
-        <div>
-          <h3>{{ $t("projects.les-chroniques.self") }}</h3>
-          <p>{{ $t("projects.les-chroniques.description") }}</p>
-          <a
-            class="btn"
-            href="https://www.youtube.com/watch?v=GuYfV7U6w8E&list=PLSBzstMGFp53S9n77CvdrQG132hMXSBBM"
-            target="_blank"
-            rel="noopener"
-          >
-            Voir
-          </a>
-        </div>
-        <img
-          src="@/assets/img/projects/les_chroniques.webp"
-          :alt="$t('projects.les-chroniques.self')"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <div>
-          <h3>{{ $t("projects.concert-polyband-2022.self") }}</h3>
-          <p>{{ $t("projects.concert-polyband-2022.description") }}</p>
-          <a
-            class="btn"
-            href="https://www.youtube.com/playlist?list=PLSBzstMGFp53nJ4RpcZImbOLHwMvDiJH4"
-            target="_blank"
-            rel="noopener"
-          >
-            Voir
-          </a>
-        </div>
-        <img
-          src="@/assets/img/projects/concert_polyband.webp"
-          :alt="$t('projects.concert-polyband-2022.self')"
-        />
-      </SplideSlide>
+    <Splide
+      class="splide"
+      :has-track="false"
+      :options="options"
+      :aria-label="$t('projects.self')"
+    >
+      <SplideTrack>
+        <SplideSlide>
+          <div>
+            <h3>{{ $t("projects.les-chroniques.self") }}</h3>
+            <p>{{ $t("projects.les-chroniques.description") }}</p>
+            <a
+              class="btn"
+              href="https://www.youtube.com/watch?v=GuYfV7U6w8E&list=PLSBzstMGFp53S9n77CvdrQG132hMXSBBM"
+              target="_blank"
+              rel="noopener"
+            >
+              Voir
+            </a>
+          </div>
+          <img
+            src="@/assets/img/projects/les_chroniques.webp"
+            :alt="$t('projects.les-chroniques.self')"
+          />
+        </SplideSlide>
+        <SplideSlide>
+          <div>
+            <h3>{{ $t("projects.concert-polyband-2022.self") }}</h3>
+            <p>{{ $t("projects.concert-polyband-2022.description") }}</p>
+            <a
+              class="btn"
+              href="https://www.youtube.com/playlist?list=PLSBzstMGFp53nJ4RpcZImbOLHwMvDiJH4"
+              target="_blank"
+              rel="noopener"
+            >
+              Voir
+            </a>
+          </div>
+          <img
+            src="@/assets/img/projects/concert_polyband.webp"
+            :alt="$t('projects.concert-polyband-2022.self')"
+          />
+        </SplideSlide>
+      </SplideTrack>
+
+      <div class="splide__arrows">
+        <button class="splide__arrow splide__arrow--prev">
+          <font-awesome-icon icon="fa-solid fa-angle-right" />
+        </button>
+        <button class="splide__arrow splide__arrow--next">
+          <font-awesome-icon icon="fa-solid fa-angle-right" />
+        </button>
+      </div>
+
+      <ul class="splide__pagination"></ul>
     </Splide>
   </article>
 </template>
 
 <script>
+import { Splide, SplideTrack, SplideSlide } from "@splidejs/vue-splide";
 import "@splidejs/vue-splide/css";
+
 export default {
   name: "ProjectsComponent",
+  components: {
+    Splide,
+    SplideTrack,
+    SplideSlide,
+  },
   data() {
     return {
       DOM: {},
@@ -57,7 +82,6 @@ export default {
         autoplay: false,
         keyboard: true,
         cover: true,
-        arrows: false,
         heightRatio: 0.4,
         speed: 350,
         rewindSpeed: this.speed,
@@ -79,12 +103,13 @@ export default {
     border-radius: var(--border-radius);
     overflow: hidden;
     box-shadow: rgba(0, 0, 0, 0.15) 0 8px 16px 0;
+    position: relative;
 
     .splide__slide {
       height: 330px !important;
 
       div {
-        padding: 4rem;
+        padding: 2rem 4rem;
         width: 100%;
         height: 100%;
         display: flex;
@@ -97,10 +122,72 @@ export default {
           rgba(0, 0, 0, 0) 70%
         );
 
+        h3,
+        p {
+          text-shadow: 0 0 0.8em var(--bg-color), 0 0 0.2em var(--bg-color);
+        }
+
         p {
           margin-bottom: 1.4rem;
         }
       }
+    }
+
+    .splide__arrow {
+      height: 100%;
+      border-radius: unset;
+      color: var(--text-color);
+      font-size: 1.2em;
+      padding: 0 1rem;
+      width: auto;
+      transition: all 250ms;
+    }
+
+    .splide__arrow--prev {
+      left: 0;
+      background: rgb(0, 0, 0);
+      background: -moz-linear-gradient(
+        90deg,
+        rgba(0, 0, 0, 0.8) 0%,
+        rgba(31, 38, 45, 0) 100%
+      );
+      background: -webkit-linear-gradient(
+        90deg,
+        rgba(0, 0, 0, 0.8) 0%,
+        rgba(31, 38, 45, 0) 100%
+      );
+      background: linear-gradient(
+        90deg,
+        rgba(0, 0, 0, 0.8) 0%,
+        rgba(31, 38, 45, 0) 100%
+      );
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#000000",endColorstr="#1f262d",GradientType=1);
+    }
+
+    .splide__arrow--next {
+      right: 0;
+      background: rgb(0, 0, 0);
+      background: -moz-linear-gradient(
+        270deg,
+        rgba(0, 0, 0, 0.8) 0%,
+        rgba(31, 38, 45, 0) 100%
+      );
+      background: -webkit-linear-gradient(
+        270deg,
+        rgba(0, 0, 0, 0.8) 0%,
+        rgba(31, 38, 45, 0) 100%
+      );
+      background: linear-gradient(
+        270deg,
+        rgba(0, 0, 0, 0.8) 0%,
+        rgba(31, 38, 45, 0) 100%
+      );
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#000000",endColorstr="#1f262d",GradientType=1);
+    }
+
+    .splide__pagination {
+      gap: 0.5rem;
+      bottom: 0.7rem;
     }
   }
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
-import VueSplide from "@splidejs/vue-splide";
 import VueObserveVisibility from "vue-observe-visibility";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -12,14 +11,13 @@ import {
   faInstagram,
   faTwitch,
 } from "@fortawesome/free-brands-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faYoutube, faTwitter, faInstagram, faTwitch, faBars);
+library.add(faYoutube, faTwitter, faInstagram, faTwitch, faBars, faAngleRight);
 
 createApp(App)
   .use(VueObserveVisibility)
   .use(router)
   .use(i18n)
-  .use(VueSplide)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
